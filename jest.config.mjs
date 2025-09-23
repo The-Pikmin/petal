71cf8d6758973bf1431013fa2d527fb7a11f023d
Jerry He <jerryhe365@gmail.com>
2025-09-23 12:55:05 -0400

    configure jest to esmodule to be compatible with eslint

---
+++ jest.config.mjs
@@ -0,0 +1,18 @@
+// jest.config.mjs
+
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+/** @type {import('jest').Config} */
+const config = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jest-environment-jsdom",
+  moduleNameMapper: {
+    "^@/components/(.*)$": "<rootDir>/components/$1",
+  },
+};
+
+export default createJestConfig(config);
